038b10736dbb3c2f88dd6520481c30e40a01e7b3
LAPTOP-DAKF2K73\Anupam <anupam.awasthi@inficaretech.com>
2018-04-11 12:12:06 +0530

    third commit

--- app/src/main/java/com/anupam/demo/MainActivity.java
+++ app/src/main/java/com/anupam/demo/MainActivity.java
@@ -1,13 +1,16 @@
 package com.anupam.demo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context=this;
     }
 }
